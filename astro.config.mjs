ec9ec0dce45f0a37fd75703d1e3e635150087425
RoboXGamer <roboxgamer52@gmail.com>
2023-02-10 07:27:31 +0000

    add integrations astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import node from "@astrojs/node";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [react(), tailwind(), partytown()]
+});
